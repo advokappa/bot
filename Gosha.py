f90c8c31fa2626d5a20d9e0e4f22f9b41abcad4c
advokappa <65288024+advokappa@users.noreply.github.com>
2020-06-09 19:38:27 +0300

    Add files via upload

---
+++ Gosha.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = commands.Bot(command_prefix = '!')
+
+@bot.event
+async def on_ready():
+	print('Bot connected')
+
+@bot.event
+async def on_member_join(member):
+	channel = bot.get_channel(667108089538805802)
+
+	role = discord.utils.get(member.guild.roles, id = 696646683072921610)
+
+	await member.add_roles(role)
+	await channel.send(f'{member.mention} присоеденился к нам!')
+
+bot.run('NzA5Mzg2ODA0NTgyODA5NjAz.Xtq4-A.owBkVN_iXMTSeub-ZcBuLtA6Xx4')
